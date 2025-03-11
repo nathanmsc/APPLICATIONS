691698ab712939a7d1bfbfb7119015d2c5ceecb4
nathanmsc <nathan@mindsetcloud.net>
2025-03-11 14:35:17 -0300

    install bootstrap

--- NUXT/nuxt.config.ts
+++ NUXT/nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   components: true,
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['usebootstrap']
 })
