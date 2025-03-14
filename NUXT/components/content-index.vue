94ecbcb5a865b69ca170fe431a572fb04012ea09
nathanmsc <nathan@mindsetcloud.net>
2025-03-13 21:59:37 -0300

    editing index

--- NUXT/components/content-index.vue
+++ NUXT/components/content-index.vue
@@ -1,63 +1,84 @@
+<script>
+</script>
 <template>
-    <Container type="fluid" id="content-index">
+    <Container type="fluid" class="content-page">
 
-         <div class="content">
-            <dt>Conectividade de dispositivos IoT</dt>
+            <dt>Conectividade de dispositivos IoT (Internet das Coisas)</dt>
   
             <dl class="blockquote">
                 <b-img
                 src="https://dummyimage.com/200x200/787878/ffffff&text=200x200"
                 thumbnail
                 alt="200x200"
+                style="max-width: 45%; max-height: 45%;"
                 />
-                <dd>A description list is perfect for defining terms.</dd>
-
+                <dd>
+                    O serviço de conectividade IoT é uma solução essencial para integrar e otimizar dispositivos inteligentes, 
+                    seja no cotidiano ou em ambientes industriais. Ele oferece uma infraestrutura escalável, permitindo a conexão 
+                    de dispositivos, sensores e sistemas à internet, promovendo automação, monitoramento e controle em tempo real.
+                </dd>
             </dl>
-        </div>
-        <div class="content">
 
-            <dt>Infraestrutura IoT nuvem</dt>
-
+            <dt>Infraestrutura em nuvem</dt>
             <dl class="blockquote">
-
                 <b-img
                 src="https://dummyimage.com/200x200/787878/ffffff&text=200x200"
                 thumbnail
                 alt="200x200"
+                style="max-width: 45%; max-height: 45%;"
                 />
-                <dd>A second definition for the same term.</dd>
-               
+                <dd>
+                    A infraestrutura de conectividade IoT na nuvem é projetada para ser escalável, permitindo que você 
+                    adicione novos dispositivos à medida que a demanda cresce, sem a necessidade de grandes 
+                    mudanças na infraestrutura existente. Isso proporciona flexibilidade e capacidade de 
+                    adaptação para futuras expansões.
+                </dd>               
             </dl>
-        </div>
         
-        <div class="content">
-
-            <dt>Infraestrutura IoT nuvem</dt>
-
+            <dt>Análise de Dados e Inteligência de Negócios</dt>
             <dl class="blockquote">
-
                 <b-img
                 src="https://dummyimage.com/200x200/787878/ffffff&text=200x200"
                 thumbnail
                 alt="200x200"
+                style="max-width: 45%; max-height: 45%;"
                 />
-                <dd>A second definition for the same term.</dd>
-               
+                <dd>
+                    Com os dispositivos IoT coletando dados continuamente, as empresas podem extrair insights 
+                    valiosos para tomada de decisões mais informadas. Isso inclui desde análise de consumo de 
+                    energia até otimização de processos logísticos, permitindo uma gestão mais inteligente e 
+                    eficaz.
+               </dd>            
             </dl>
-        </div>  
+
+            <dt>Análise de Dados e Inteligência de Negócios</dt>
+            <dl class="blockquote">
+                <b-img
+                src="https://dummyimage.com/200x200/787878/ffffff&text=200x200"
+                thumbnail
+                alt="200x200"
+                style="max-width: 45%; max-height: 45%;"
+                />
+                <dd>
+                    Com os dispositivos IoT coletando dados continuamente, as empresas podem extrair insights 
+                    valiosos para tomada de decisões mais informadas. Isso inclui desde análise de consumo de 
+                    energia até otimização de processos logísticos, permitindo uma gestão mais inteligente e 
+                    eficaz.
+               </dd>            
+            </dl>
             
     </Container>
 </template>
 <style>
-#content-index{
+.content-page{
     background-color: bisque;
-    padding: 3%;
+    padding: 3% 8% 5% 8%;
     color:azure;
     align-items: center;
 }
 dl{
     display: flex;
-    align-items: center;
+    align-items:flex;
 
 }
 dt {
@@ -68,5 +89,9 @@ dt {
 
 dd {
   background-color: blueviolet;
+  text-align:justify;
+  text-justify:auto;
+  align-items: center;
+  padding: 2%;
 }
 </style>
